5e165fcad239fefe6c335ad03d7d47107124261a
Sergey Maksimov <s.mksimov@gmail.com>
2022-11-01 11:56:07 +0300

    add storiesIds fetching

--- src/pages/StoriesPage/StoriesPage.tsx
+++ src/pages/StoriesPage/StoriesPage.tsx
@@ -1,33 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import { getStoriesIds } from '../../api';
 
-const stubStories = [
-  {
-    id: 1,
-    header: 'number1',
-    text: 'hello from 1'
-  },
-  {
-    id: 2,
-    header: 'number2',
-    text: 'hello from 2'
-  },
-  {
-    id: 3,
-    header: 'number3',
-    text: 'hello from 3'
-  },
-]
 
 export const StoriesPage = () => {
+  const [storiesIds, setStoriesIds] = useState([]);
+
+  useEffect(() => {
+    getStoriesIds()
+      .then(res => {
+        setStoriesIds(res);
+      })
+  }, []);
+
   return (
     <div>
-      {stubStories.map(({id, header, text}) => (
-        <div key={id}>
-          Header: {header}<br />
-          Text: {text}<br />
+      {storiesIds.map(id => (
+        <div key={id}>That is is number {id}
           <NavLink to={`story/${id}`}>Link</NavLink>
         </div>
+
       ))}
     </div>
   );
