d1f4f2ed1a559830c643985a5e97d04ef61cae31
Sergey Maksimov <s.mksimov@gmail.com>
2022-11-01 13:37:12 +0300

    add cors support

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 
 const PORT = 5000;
 const app = express();
 
+app.use(cors());
+
 app.get("/api", (req, res) => {
-  res.send("hello !!!!");
+  res.send("hello !");
 });
 
-app.listen(PORT, () => console.log(`start listening on port : ${PORT}`));
+app.listen(PORT, () => console.log(`server start on port : ${PORT}`));
