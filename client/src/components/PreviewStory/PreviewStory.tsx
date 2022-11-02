85fc7368bc1e97713b8cda3704d1465f2a033a66
Sergey Maksimov <s.mksimov@gmail.com>
2022-11-02 14:28:36 +0300

    add PreviewStory styles

--- client/src/components/PreviewStory/PreviewStory.tsx
+++ client/src/components/PreviewStory/PreviewStory.tsx
@@ -1,17 +1,39 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { formatDistanceToNow } from 'date-fns';
+import s from './PreviewStory.module.css';
+import { Divider } from '../Divider';
 
 export const PreviewStory = ({ story }: any) => {
-  const { id, by, title } = story;
+  const { id, by, title, score } = story;
 
   return (
-    <div>
+    <div className={s.container}>
       <h4>
         <NavLink to={`story/${id}`}>
           {title}
         </NavLink>
       </h4>
-      by {by} - {title}
+      <div>
+        {score} points by {by}
+        <Divider />
+        {story.time && formatDistanceToNow(new Date(story.time * 1000))} ago
+        {story.kids?.length &&
+          <span>
+        <Divider />
+            {story.descendants > 0
+              ?
+              <NavLink to={`story/${id}`}>
+                {story.descendants} comments
+              </NavLink>
+              :
+              <>
+                {story.descendants} comments
+              </>
+            }
+        </span>
+        }
+      </div>
     </div>
   );
 };
