45328c1672d1e0f447e24d0cc26454f8e7d787a5
Sergey Maksimov <s.mksimov@gmail.com>
2022-11-02 10:32:55 +0300

    add types

---
+++ client/src/types/types.ts
@@ -0,0 +1,29 @@
+export type StoryState = {
+  stories: StoryType[]
+  story: StoryType
+  loading: boolean
+  error: null | string
+};
+
+export type StoryType = {
+  by?: string
+  descendants?: number
+  id?: number
+  kids?: Array<number>
+  score?: number
+  time?: number
+  title?: string
+  type?: string
+  url?: string
+};
+
+export type CommentType = {
+  by?: string
+  id?: number
+  kids?: Array<number>
+  parent?: number
+  text?: string
+  time?: number
+  type?: string
+  deleted?: boolean
+};
