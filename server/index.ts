cec4811f488795b26278b5b4d823c98d28078c36
Sergey Maksimov <s.mksimov@gmail.com>
2022-11-03 18:50:33 +0300

    refactor index on server

--- server/index.ts
+++ server/index.ts
@@ -6,18 +6,10 @@ import storyRoutes from './src/StoryRoutes.js';
 
 const PORT = 5000;
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const isProduction = process.env.NODE_ENV === 'production'
 const app: Express = express();
 
 app.use(cors());
+app.use(express.static(path.join(__dirname, "../../client/dist")));
 app.use('/api/stories', storyRoutes);
 
-if (isProduction) {
-  app.use(express.static(path.join(__dirname, "../client/dist")));
-}
-
-app.use('*', (_, res) => {
-  res.status(404).json({ message: 'Page not found' })
-})
-
-app.listen(PORT, () => console.log(`server start on port : ${PORT}`));
+app.listen(PORT, () => console.log(`server start on port : ${PORT} \n Local: http://127.0.0.1:${PORT}`));
