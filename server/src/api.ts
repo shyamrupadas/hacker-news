996b7d5972c3d22f813c8d33d60c09ffefb791a4
Sergey Maksimov <s.mksimov@gmail.com>
2022-11-03 22:59:54 +0300

    add api to server

---
+++ server/src/api.ts
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://hacker-news.firebaseio.com/v0/';
+const STORIES_URL = `${BASE_URL}newstories.json`;
+const ITEM_URL = `${BASE_URL}item/`;
+
+export const getStories = () => (
+  axios
+    .get(STORIES_URL)
+    .then(res => res.data)
+);
+
+export const getItem = (itemId: string) => (
+  axios
+    .get(`${ITEM_URL}${itemId}.json`)
+    .then(res => res.data)
+);
