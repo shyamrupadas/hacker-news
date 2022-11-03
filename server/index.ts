99a7086facba2cb091a75673175c152a3834ce46
Sergey Maksimov <s.mksimov@gmail.com>
2022-11-03 14:56:14 +0300

    add controller

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,9 @@
-import express from 'express';
+import express, { Express, Response } from 'express';
 import cors from 'cors';
 import { fileURLToPath } from 'url';
 import path from 'path';
 import axios from 'axios';
+import { funcMe } from './src/controller.js';
 
 const PORT = 5000;
 
@@ -10,7 +11,9 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const isProduction = process.env.NODE_ENV === 'production'
 
-const app = express();
+const app: Express = express();
+
+funcMe();
 
 app.use(cors());
 
@@ -18,11 +21,11 @@ if (isProduction) {
   app.use(express.static(path.join(__dirname, "../client/dist")));
 }
 
-app.get("/api/stories", async (req, res) => {
+app.get("/api/stories", async (_, res: Response) => {
   try {
     const response = await axios.get('https://hacker-news.firebaseio.com/v0/newstories.json');
 
-    const promises = response.data.slice(0, 100).map((storyId) => (
+    const promises = response.data.slice(0, 100).map((storyId: number) => (
         axios.get(`https://hacker-news.firebaseio.com/v0/item/${storyId}.json`).then(res => res.data)
       )
     );
@@ -33,7 +36,7 @@ app.get("/api/stories", async (req, res) => {
   }
 });
 
-app.use('*', (req, res) => {
+app.use('*', (_, res) => {
   res.status(404).json({ message: 'Page not found' })
 })
 
---
+++ server/src/controller.ts
@@ -0,0 +1,1 @@
+export const funcMe = () => 'hello from me';
