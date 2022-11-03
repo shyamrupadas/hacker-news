02563800d21923520fc19d2cfebd975bd5543847
Sergey Maksimov <s.mksimov@gmail.com>
2022-11-03 23:00:32 +0300

    refactor StoryController

--- server/src/StoryController.ts
+++ server/src/StoryController.ts
@@ -1,40 +1,30 @@
 import { Request, Response } from 'express';
-import axios from 'axios';
-
-const BASE_URL = 'https://hacker-news.firebaseio.com/v0/';
-const STORIES_URL = `${BASE_URL}newstories.json`;
-const STORY_URL = `${BASE_URL}item/`;
+import { getItem, getStories } from './api.js';
 
 class StoryController {
   async getAll(_, res: Response) {
     try {
-      const response = await axios.get(STORIES_URL);
+      const response = await getStories();
 
-      const promises = response.data
-        .slice(0, 100)
-        .map((storyId: number) => (
-            axios.get(`${STORY_URL}${storyId}.json`)
-              .then(res => res.data)
-          )
-        );
+      const promises = response.slice(0, 100)
+        .map((storyId: string) => getItem(storyId));
 
       const result = await Promise.all(promises);
       res.json(result);
     } catch (e) {
       res.status(500).json(e);
     }
-  }
+  };
 
   async getOne(req: Request, res: Response) {
     try {
-      const result = await axios.get(`${STORY_URL}${req.params.id}.json`)
-        .then(res => res.data);
+      const result = await getItem(req.params.id);
 
       res.json(result);
     } catch (e) {
       res.status(500).json(e);
     }
-  }
+  };
 }
 
 export default new StoryController();
